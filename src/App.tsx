b7bc360070b5ec20efe1ebc28ee1de131f5e0fba
slogup-dale <dyddn304@gmail.com>
2022-06-09 15:31:43 +0900

    add: 초기세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+
+const { kakao, naver }: any = window;
 
 function App() {
+  useEffect(() => {
+    const container = document.getElementById("kakaoMap");
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, options);
+  }, []);
+
+  useEffect(() => {
+    const container = document.getElementById("naverMap");
+    const mapOptions = {
+      center: new naver.maps.LatLng(37.3595704, 127.105399),
+      zoom: 10,
+    };
+
+    const map = new naver.maps.Map(container, mapOptions);
+  }, []);
+
+  useEffect(() => {
+    naver.maps.Service.geocode(
+      { query: "불정로 6" },
+      function (status: any, response: any) {
+        if (status !== naver.maps.Service.Status.OK) {
+          return alert("Something wrong!");
+        }
+
+        var result = response.v2,
+          items = result.addresses;
+      }
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main style={{ display: "flex" }}>
+      <article>
+        <h1>카카오 지도</h1>
+        <div
+          id="kakaoMap"
+          style={{
+            width: "500px",
+            height: "500px",
+          }}
+        ></div>
+      </article>
+
+      <article>
+        <h1>네이버 지도</h1>
+        <div
+          id="naverMap"
+          style={{
+            width: "500px",
+            height: "500px",
+          }}
+        ></div>
+      </article>
+    </main>
   );
 }
 
